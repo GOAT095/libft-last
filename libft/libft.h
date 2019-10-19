c20b610262c7e15e2b8c4f1a4571449a531a9980
Abdelhakim Nassif <anassif@e1r1p5.1337.ma>
2019-10-19 17:51:19 +0200

    5.51-19/10

--- libft/libft.h
+++ libft/libft.h
@@ -49,4 +49,11 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
+
+typedef struct		s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
 #endif
